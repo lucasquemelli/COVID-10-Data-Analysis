0ac630ee8344c0f6bf519e7d9ea863a9d0ed3cd1
Lucas Quemelli <lucasquemelli@gmail.com>
2022-12-24 11:40:55 -0300

    Add files via upload

--- covid_data_exploring.sql
+++ covid_data_exploring.sql
@@ -33,6 +33,12 @@ SELECT DISTINCT
 FROM covid_deaths 
 WHERE (continent IS NULL OR continent = "");
 
+SELECT DISTINCT
+	continent,
+	location
+FROM covid_deaths 
+WHERE LOWER(location) = "africa";
+
 
 -- Selecting data we are going to work with
 
@@ -44,4 +50,38 @@ SELECT
 	total_deaths, 
 	population
 FROM covid_deaths
-WHERE (continent IS NOT NULL OR continent != "");
+WHERE (continent IS NOT NULL OR continent != "")
+ORDER BY 1,2;
+
+-- Total cases vs total deaths
+
+SELECT
+	location, 
+	date, 
+	total_cases, 
+	new_cases, 
+	total_deaths, 
+	population,
+	ROUND((total_deaths/total_cases)*100, 2) AS percentage_death_case,
+	ROUND((total_deaths/population)*100, 5) AS percentage_death_population
+FROM covid_deaths
+WHERE (continent IS NOT NULL OR continent != "")
+GROUP BY 1,2,3,4,5,6
+ORDER BY 1,2;
+
+-- Total cases vs total deaths in Brazil
+
+SELECT
+	location, 
+	date, 
+	total_cases, 
+	new_cases, 
+	total_deaths, 
+	population,
+	ROUND((total_deaths/total_cases)*100, 2) AS percentage_death_case,
+	ROUND((total_deaths/population)*100, 5) AS percentage_death_population
+FROM covid_deaths
+WHERE LOWER(location) = "brazil"
+GROUP BY 1,2,3,4,5,6
+ORDER BY 1,2;
+
